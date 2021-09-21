4969f23c24fbf43fca1253fd11c6ef95b00ba32d
Lucas Samel <lucas.samel97@gmail.com>
2021-09-21 17:49:33 -0300

    recursivo

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 V = ['tipo 1', 'tipo 2', 'tipo 3', 'tipo 4', 'tipo 5', 'tipo 1', 'tipo 1', 'tipo 3']
 
 
-def rec(dado, vetor):
-    if dado in vetor:
-        return
+def rec(dado, vetor, i, j):
+    if i >= (len(vetor)):
+        return print("O dado", dado, "foi encontrado ", j, "vezes.")
+    elif vetor[i] == dado:
+        return rec(dado, vetor, i + 1, j + 1)
+    return rec(dado, vetor, i + 1, j)
 
 
 def iterativa(vetor, dado):
     j = 0
     for i in vetor:
         if i == dado:
-           j += 1
+            j += 1
     print("O dado", dado, "foi encontrado ", j, "vezes.")
 
 
 if __name__ == '__main__':
+    # dado = input("Digite o tipo de dado que deseja verificar: ")
+    dado = "tipo 1"
+    rec(dado, V, 0, 0)
 
-    dado = input("Digite o tipo de dado que deseja verificar: ")
     iterativa(V, dado)
 
-    rec(dado, V)
+
+
