af6f081c42174918bab6abc2c8c403dc0346eb57
Lucas Samel <lucas.samel97@gmail.com>
2021-09-21 16:45:53 -0300

    iterativa

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-
 V = ['tipo 1', 'tipo 2', 'tipo 3', 'tipo 4', 'tipo 5', 'tipo 1', 'tipo 1', 'tipo 3']
 
-def rec(vetor):
-    pass
 
-def iterativa(vetor, lista):
+def rec(dado, vetor):
+    if dado in vetor:
+        return
+
+
+def iterativa(vetor, dado):
+    j = 0
     for i in vetor:
-        if i in lista:
-            pass
-        else:
-            lista.append(i)
+        if i == dado:
+           j += 1
+    print("O dado", dado, "foi encontrado ", j, "vezes.")
 
 
 if __name__ == '__main__':
-    rec(V)
 
-    listaV = []
-    iterativa(V, listaV)
+    dado = input("Digite o tipo de dado que deseja verificar: ")
+    iterativa(V, dado)
+
+    rec(dado, V)
