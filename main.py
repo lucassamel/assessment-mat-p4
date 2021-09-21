459e105effcd6aad6c3348f9217cd17bf093cb8d
Lucas Samel <lucas.samel97@gmail.com>
2021-09-21 16:32:58 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+
+V = ['tipo 1', 'tipo 2', 'tipo 3', 'tipo 4', 'tipo 5', 'tipo 1', 'tipo 1', 'tipo 3']
+
+def rec(vetor):
+    pass
+
+def iterativa(vetor, lista):
+    for i in vetor:
+        if i in lista:
+            pass
+        else:
+            lista.append(i)
+
+
+if __name__ == '__main__':
+    rec(V)
+
+    listaV = []
+    iterativa(V, listaV)
